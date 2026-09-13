9557378fbf6bf1e3a9864d0bbe7f488a18e35df3
fiksr <67743991+fiksr@users.noreply.github.com>
2026-09-13 16:57:08 +0200

    fix(parser): clean HTML sanitization string syntax

--- parser.lua
+++ parser.lua
@@ -40,10 +40,7 @@ end
 
 function Parser.stripHtml(html)
     if not html then return "" end
-    local s = unescapeXml(html):gsub("[ -¤][
-8-91][
-8-91][
-8-91]", ""):gsub("984[d-9]", "")
+    local s = unescapeXml(html)
     -- Remove scripts, styles, iframes
     s = s:gsub("<script.-</script>", "")
          :gsub("<style.-</style>", "")
